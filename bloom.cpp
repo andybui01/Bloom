7421d1e4cf0908749c4082c28010feb3d6412f10
Andy Bui <andy.bui2001@gmail.com>
2020-11-01 23:56:40 +1100

    add rounding macros

--- bloom.cpp
+++ bloom.cpp
@@ -1,27 +1,3 @@
-/**
- * MIT License
- *
- * Copyright (c) 2020 Andy Bui
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- **/
-
 #include <bloom.hpp>
 #include <stdlib.h>
 #include <string.h>
@@ -42,6 +18,12 @@ BloomFilter::BloomFilter(uint32_t elements, double fp_rate): elements(elements),
     num_hash = calculateNumHash(elements, size);
 }
 
+// void BloomFilter::insert(char* str) {
+//     for (int i = 0; i < num_hash; ++i) {
+//         uint64_t bit_index = hash(str, salts[i]) % (size * 32);
+//     }
+// }
+
 double BloomFilter::getSize() {
     return size;
 }
@@ -55,10 +37,11 @@ int BloomFilter::getNumHash() {
  **/
 
 uint32_t BloomFilter::calculateSize(uint32_t elements, double fp_rate) {
-    return -(elements * log(fp_rate)) / pow(log(2), 2);
+    return ROUND_UP(-(elements * log(fp_rate)) / pow(log(2), 2), 32);
 }
-double BloomFilter::calculateNumHash(uint32_t elements, uint32_t size) {
-    return size * log(2) / elements;
+
+int BloomFilter::calculateNumHash(uint32_t elements, uint32_t size) {
+    return CEILING(size * log(2), elements);
 }
 
 // Hash function that implements Dan Bernstein's djb2:
--- bloom.hpp
+++ bloom.hpp
@@ -1,29 +1,8 @@
-/**
- * MIT License
- *
- * Copyright (c) 2020 Andy Bui
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- **/
-
 #ifndef BLOOM_HPP
 #define BLOOM_HPP
+
+#define ROUND_UP(x, y) ((((x) + (y) - 1) / (y)) * (y))
+#define CEILING(x, y) (((x) + (y) - 1) / (y))
 
 #include <math.h>
 #include <iostream>
@@ -43,6 +22,9 @@ private:
 
     // False-positive rate
     double fp_rate;
+
+    // Bitmap
+    uint32_t bit_map;
 
     // Salts used for hashing
     const char* salts[50] = {
@@ -99,16 +81,17 @@ private:
     };
 
     uint32_t calculateSize(uint32_t elements, double fp_rate);
-    double calculateNumHash(uint32_t elements, uint32_t size);
+    int calculateNumHash(uint32_t elements, uint32_t size);
+    uint64_t hash(char* str, const char* salt);
 
 
 public:
     BloomFilter(uint32_t elements);
     BloomFilter(uint32_t elements, double fp_rate);
+    void insert(char* str);
     double getSize();
     int getNumHash();
 
-    uint64_t hash(char* str, const char* salt);
 };
 
 #endif
